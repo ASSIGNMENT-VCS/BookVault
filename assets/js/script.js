1dbd7dab9d1fd2872b3083b7914aa7b482e7566f
khalifalmahmud0 <dev.env.khalif@gmail.com>
2024-10-15 17:53:56 +0600

    feat: write a js function to get data & simply show data in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("Allah is the Most Powerful.");
+const APIURL = "https://gutendex.com/books";
+const BOOKLISTS = [];
+
+let books = async () => {
+  let response = await fetch(APIURL);
+  let data = await response.json();
+  return data;
+};
+
+async function fetchAndPopulateBooks() {
+  try {
+    const data = await books();
+    BOOKLISTS.push(data);
+    console.log("Books fetched and stored globally:", BOOKLISTS);
+  } catch (error) {
+    console.error("Error fetching books:", error);
+  }
+}
+
+fetchAndPopulateBooks();
