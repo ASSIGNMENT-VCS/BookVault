52ad0cda4901f1adaa61984aaa9fd488c3136847
khalifalmahmud0 <dev.env.khalif@gmail.com>
2024-10-18 12:34:44 +0600

    feat: Implement Product Filter Functionality | fix: Fix Image Not Found Issue | chore: remove unnecessary codes from JS , Optimize Function & Array filter , update few CSS code

--- assets/js/utils.js
+++
@@ -1,7 +0,0 @@
-export const ROOTAPIURL = "https://gutendex.com";
-
-export const FetchData = async (APIURL) => {
-  let response = await fetch(APIURL);
-  let data = await response.json();
-  return data;
-};
